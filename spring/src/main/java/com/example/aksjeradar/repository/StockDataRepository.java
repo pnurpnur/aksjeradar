368157a1230abbe741fb14954d1bdbc72a023345
Inge Pettersen <i.pettersen@sportradar.com>
2026-03-17 19:31:09 +0100

    Removed spring

--- spring/src/main/java/com/example/aksjeradar/repository/StockDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aksjeradar.repository;
-
-import com.example.aksjeradar.model.StockData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockDataRepository extends JpaRepository<StockData, String> {
-}
